45e4805a2cd19f68620419394473f86b2a1ea5ae
DJeovannyZ <darwinzaldana@gmail.com>
2024-06-11 22:54:48 -0600

    ZA20003: startup para windows

---
+++ startup.bat
@@ -0,0 +1,8 @@
+@echo off
+mvn clean package -DskipTests
+
+echo "Deteniendo los contenedores y borrando imagenes creadas previamente..."
+docker-compose down --rmi local --volumes
+
+echo "Iniciando la aplicacion..."
+docker-compose up
